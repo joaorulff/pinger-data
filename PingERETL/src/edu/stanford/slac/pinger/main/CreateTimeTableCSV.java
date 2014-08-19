44b96446145492917d9372bbdb0d2e533627dc3c
cristianeceia <cristianeceia@gmail.com>
2014-08-19 02:50:59 +0000

    Alterando a ordem em que as linhas do arquivo timeTable.csv são geradas.

--- PingERETL/src/edu/stanford/slac/pinger/main/CreateTimeTableCSV.java
+++ PingERETL/src/edu/stanford/slac/pinger/main/CreateTimeTableCSV.java
@@ -11,6 +11,28 @@ public class CreateTimeTableCSV {
 
 	public static final String NULL_VALUE = "\\N";
 	
+	private static Calendar setInitialDate(){
+		Calendar date = Calendar.getInstance();
+		
+		date.set(Calendar.DAY_OF_MONTH, 1);
+		date.set(Calendar.MONTH, Calendar.JANUARY);
+		date.set(Calendar.YEAR, 1998);
+		
+		return date;
+	}
+	
+	private static Calendar setFinalDate(){
+		Calendar date = Calendar.getInstance();
+		
+		int currentYear = date.get(Calendar.YEAR);
+		
+		date.set(Calendar.DAY_OF_MONTH, 1);
+		date.set(Calendar.MONTH, Calendar.JANUARY);
+		date.set(Calendar.YEAR, currentYear+1);
+		
+		return date;
+	}
+	
 	public static void main(String[] args) {
 		String timeTableFilePath = "./databaseTablesCSV/timeTable.csv";
 		
@@ -27,50 +49,51 @@ public class CreateTimeTableCSV {
 		String monthShortName = null;
 		String yearShortFormat = null;
 		
+		NumberFormat f = new DecimalFormat("00"); 
+		
 		String timeTableContent = "ID,Year,Month,Day,Unit,TimeStamp,Label\n";
 		
-		Calendar initialDate = Calendar.getInstance();
-		Calendar finalDate = Calendar.getInstance();
+		Calendar date = setInitialDate();		
+		Calendar finalDate = setFinalDate();
 		
-		initialDate.set(Calendar.DAY_OF_MONTH, 1);
-		initialDate.set(Calendar.MONTH, Calendar.JANUARY);
-		initialDate.set(Calendar.YEAR, 1998);
-		
-		finalDate.set(Calendar.DAY_OF_MONTH, 1);
-		finalDate.set(Calendar.MONTH, Calendar.JANUARY);
-		finalDate.set(Calendar.YEAR, 2015);
-        
-        NumberFormat f = new DecimalFormat("00");        
-        
-        while (initialDate.getTime().before(finalDate.getTime())){
-        	
-        	/* ***** Day Granularity ***** */
+		/* ***** Day Granularity ***** */
+		while (date.getTime().before(finalDate.getTime())){			
         	id++;        	
-            year = String.valueOf(initialDate.get(Calendar.YEAR));
+            year = String.valueOf(date.get(Calendar.YEAR));
             yearShortFormat = year.substring(2, 4);
-            month = f.format(initialDate.get(Calendar.MONTH)+1); //A month is represented by an integer from 0 to 11
-            monthShortName = initialDate.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
-            day = f.format(initialDate.get(Calendar.DAY_OF_MONTH));
+            month = f.format(date.get(Calendar.MONTH)+1); //A month is represented by an integer from 0 to 11
+            monthShortName = date.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
+            day = f.format(date.get(Calendar.DAY_OF_MONTH));
             unit = "Day";
             timeStamp = year + "-" + month + "-" + day;
             label = yearShortFormat + monthShortName + day;
             timeTableContent += id + "," + year + "," + month + "," + day + "," + unit + "," + timeStamp + "," + label + "\n";
             
-            /* ***** Month Granularity ***** */
+            date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH)+1); //Next day
+		}
+		
+		/* ***** Month Granularity ***** */
+		date = setInitialDate();
+		while (date.getTime().before(finalDate.getTime())){			
         	id++;        	
-            year = String.valueOf(initialDate.get(Calendar.YEAR));
+            year = String.valueOf(date.get(Calendar.YEAR));
             yearShortFormat = year.substring(2, 4);
-            month = f.format(initialDate.get(Calendar.MONTH)+1); //A month is represented by an integer from 0 to 11
-            monthShortName = initialDate.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
+            month = f.format(date.get(Calendar.MONTH)+1); //A month is represented by an integer from 0 to 11
+            monthShortName = date.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
             day = NULL_VALUE;
             unit = "Month";
             timeStamp = year + "-" + month;
             label = monthShortName + year;
             timeTableContent += id + "," + year + "," + month + "," + day + "," + unit + "," + timeStamp + "," + label + "\n";
             
-            /* ***** Year Granularity ***** */
+            date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH)+1); //Next day
+		}
+        
+		/* ***** Year Granularity ***** */
+		date = setInitialDate();
+		while (date.getTime().before(finalDate.getTime())){			
         	id++;        	
-            year = String.valueOf(initialDate.get(Calendar.YEAR));
+            year = String.valueOf(date.get(Calendar.YEAR));
             month = NULL_VALUE;
             day = NULL_VALUE;
             unit = "Year";
@@ -78,8 +101,8 @@ public class CreateTimeTableCSV {
             label = year;
             timeTableContent += id + "," + year + "," + month + "," + day + "," + unit + "," + timeStamp + "," + label + "\n";
             
-            initialDate.set(Calendar.DAY_OF_MONTH, initialDate.get(Calendar.DAY_OF_MONTH)+1); //Next day
-        }
+            date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH)+1); //Next day
+		}
         
         Utils.createFileGrantingPermissions(timeTableFilePath);
         Utils.writeIntoFile(timeTableContent, timeTableFilePath);
