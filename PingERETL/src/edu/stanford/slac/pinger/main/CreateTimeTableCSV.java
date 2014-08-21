3b64dbf3f677629e45488b3d2b3f68cc02d80b72
cristianeceia <cristianeceia@gmail.com>
2014-08-21 01:51:27 +0000

    Alterando o valor da coluna Unit da tabela Time: granularidade representada por um inteiro.

---
+++ PingERETL/src/edu/stanford/slac/pinger/main/CreateTimeTableCSV.java
@@ -0,0 +1,117 @@
+package edu.stanford.slac.pinger.main;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import edu.stanford.slac.pinger.general.utils.Utils;
+
+public class CreateTimeTableCSV {
+
+	public static final String NULL_VALUE = "\\N";
+	
+	public static final int YEAR_GRANULARITY = 1;
+	public static final int MONTH_GRANULARITY = 2;
+	public static final int DAY_GRANULARITY = 3;
+	
+	
+	private static Calendar setInitialDate(){
+		Calendar date = Calendar.getInstance();
+		
+		date.set(Calendar.DAY_OF_MONTH, 1);
+		date.set(Calendar.MONTH, Calendar.JANUARY);
+		date.set(Calendar.YEAR, 1998);
+		
+		return date;
+	}
+	
+	private static Calendar setFinalDate(){
+		Calendar date = Calendar.getInstance();
+		
+		int currentYear = date.get(Calendar.YEAR);
+		
+		date.set(Calendar.DAY_OF_MONTH, 1);
+		date.set(Calendar.MONTH, Calendar.JANUARY);
+		date.set(Calendar.YEAR, currentYear+1);
+		
+		return date;
+	}
+	
+	public static void main(String[] args) {
+		String timeTableFilePath = "./databaseTablesCSV/timeTable.csv";
+		
+		//Time table fields
+		int id = 0;
+		String year = null;
+		String month = null;
+		String day = null;
+		//String hour = null;
+		int unit = 0;
+		String timeStamp = null;
+		String label = null;
+		
+		String monthShortName = null;
+		String yearShortFormat = null;
+		
+		NumberFormat f = new DecimalFormat("00"); 
+		
+		String timeTableContent = "ID,Year,Month,Day,Unit,TimeStamp,Label\n";
+		
+		Calendar date = setInitialDate();		
+		Calendar finalDate = setFinalDate();
+		
+		/* ***** Day Granularity ***** */
+		while (date.getTime().before(finalDate.getTime())){			
+        	id++;        	
+            year = String.valueOf(date.get(Calendar.YEAR));
+            yearShortFormat = year.substring(2, 4);
+            month = f.format(date.get(Calendar.MONTH)+1); //A month is represented by an integer from 0 to 11
+            monthShortName = date.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
+            day = f.format(date.get(Calendar.DAY_OF_MONTH));
+            unit = DAY_GRANULARITY;
+            timeStamp = year + "-" + month + "-" + day;
+            label = yearShortFormat + monthShortName + day;
+            timeTableContent += id + "," + year + "," + month + "," + day + "," + unit + "," + timeStamp + "," + label + "\n";
+            
+            date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH)+1); //Next day
+		}
+		
+		/* ***** Month Granularity ***** */
+		date = setInitialDate();
+		while (date.getTime().before(finalDate.getTime())){			
+        	id++;        	
+            year = String.valueOf(date.get(Calendar.YEAR));
+            yearShortFormat = year.substring(2, 4);
+            month = f.format(date.get(Calendar.MONTH)+1); //A month is represented by an integer from 0 to 11
+            monthShortName = date.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.ENGLISH);
+            day = NULL_VALUE;
+            unit = MONTH_GRANULARITY;
+            timeStamp = year + "-" + month;
+            label = monthShortName + year;
+            timeTableContent += id + "," + year + "," + month + "," + day + "," + unit + "," + timeStamp + "," + label + "\n";
+            
+            date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH)+1); //Next day
+		}
+        
+		/* ***** Year Granularity ***** */
+		date = setInitialDate();
+		while (date.getTime().before(finalDate.getTime())){			
+        	id++;        	
+            year = String.valueOf(date.get(Calendar.YEAR));
+            month = NULL_VALUE;
+            day = NULL_VALUE;
+            unit = YEAR_GRANULARITY;
+            timeStamp = year;
+            label = year;
+            timeTableContent += id + "," + year + "," + month + "," + day + "," + unit + "," + timeStamp + "," + label + "\n";
+            
+            date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH)+1); //Next day
+		}
+        
+        Utils.createFileGrantingPermissions(timeTableFilePath);
+        Utils.writeIntoFile(timeTableContent, timeTableFilePath);
+		
+	}
+
+}
