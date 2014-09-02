8b013a76e2c7fa181fb863ff85504673868a7de9
cristianeceia <cristianeceia@gmail.com>
2014-09-02 03:58:52 +0000

    Alteração na classe geradora do arquivo NodeDetails.json

--- PingERETL/src/edu/stanford/slac/pinger/main/pre/CreateNodeDetailsJson.java
+++ PingERETL/src/edu/stanford/slac/pinger/main/pre/CreateNodeDetailsJson.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-import edu.stanford.slac.pinger.general.C;
 import edu.stanford.slac.pinger.general.Logger;
 import edu.stanford.slac.pinger.general.utils.Utils;
 
@@ -15,8 +14,9 @@ public class CreateNodeDetailsJson {
 	public static void main(String[] args)  {
 		String nodeDetails = "";
 		String urlContent = "";
+		
 		try {
-			URL nodesCfUrl = new URL(C.NODE_DETAILS_CF);
+			URL nodesCfUrl = new URL("http://www-iepm.slac.stanford.edu/pinger/pingerworld/nodes.cf");
 			URLConnection uc = nodesCfUrl.openConnection();
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
@@ -79,78 +79,79 @@ public class CreateNodeDetailsJson {
 					value = nodeLineInfo[j].trim();
 
 					switch (j) {
-					case 0:
-						nodeID++;
-						nodeDetailsContent += "\t{\n\t\t\"NodeID\":\"" + nodeID + "\",\n"
-								+ "\t\t\"NodeName\":" + nodeName + ",\n"
-								+ "\t\t\"NodeIP\":" + value.substring(1).trim() + "\",\n";	//Ignore the initial "["
-						break;
-					case 1:
-						nodeDetailsContent += "\t\t\"NodeSiteName\":" + value + "\",\n";
-						break;
-					case 2:
-						nodeDetailsContent += "\t\t\"NodeNickName\":" + value + "\",\n";
-						break;
-					case 3:
-						nodeDetailsContent += "\t\t\"NodeFullName\":" + value + "\",\n";
-						break;
-					case 4:
-						nodeDetailsContent += "\t\t\"LocationDescription\":" + value + "\",\n";
-						break;
-					case 5:
-						nodeDetailsContent += "\t\t\"Country\":" + value + "\",\n";
-						break;
-					case 6:
-						nodeDetailsContent += "\t\t\"Continent\":" + value + "\",\n";
-						break;
-					case 7:
-						if (!value.contains("\"\"") && !value.contains("NOT-SET")) {
-							String[] coord = value.split(" ");
-							nodeDetailsContent += "\t\t\"Latitude\":" + coord[0].trim() + "\",\n"
-									+ "\t\t\"Longitude\":\"" + coord[1].trim() + "\",\n";
-						} else {
-							nodeDetailsContent += "\t\t\"Latitude\":" + "\"\" " + "\",\n"
-									+ "\t\t\"Longitude\":\"" + "\"\" " + "\",\n";
-						}
-						break;
-					case 8:
-						nodeDetailsContent += "\t\t\"ProjectType\":" + value + "\",\n";
-						break;
-					case 9:
-						nodeDetailsContent += "\t\t\"PingServer\":" + value + "\",\n";
-						break;
-					case 10:
-						nodeDetailsContent += "\t\t\"TraceServer\":" + value + "\",\n";
-						break;
-					case 11:
-						nodeDetailsContent += "\t\t\"DataServer\":" + value + "\",\n";
-						break;
-					case 12:
-						nodeDetailsContent += "\t\t\"NodeURL\":" + value + "\",\n";
-						break;
-					case 13:
-						nodeDetailsContent += "\t\t\"NodeGMT\":" + value + "\",\n";
-						break;
-					case 14:
-						nodeDetailsContent += "\t\t\"Group\":" + value + "\",\n";
-						break;
-					case 15:
-						nodeDetailsContent += "\t\t\"AppUser\":" + value + "\",\n";
-						break;
-					case 16:
-						nodeDetailsContent += "\t\t\"ContactInformation\":" + value + "\",\n";
-						break;
-					case 17:
-						if (value.contains("\n")) {
-							value = value.replace("\n", " ");
-						}
-						nodeDetailsContent += "\t\t\"NodeComments\":" + value.substring(0, value.length()-1) + "\n";	//The last line of node description doesn't end with comma
-						break;
-					default:
-						break;
+						case 0:
+							nodeID++;
+							nodeDetailsContent += "\t" + nodeName + ": {\n"
+												+ "\t\t\"NodeID\":\"" + nodeID + "\",\n"
+												+ "\t\t\"NodeName\":" + nodeName + ",\n"
+												+ "\t\t\"NodeIP\":" + value.substring(1).trim() + "\",\n";	//Ignore the initial "["
+							break;
+						case 1:
+							nodeDetailsContent += "\t\t\"NodeSiteName\":" + value + "\",\n";
+							break;
+						case 2:
+							nodeDetailsContent += "\t\t\"NodeNickName\":" + value + "\",\n";
+							break;
+						case 3:
+							nodeDetailsContent += "\t\t\"NodeFullName\":" + value + "\",\n";
+							break;
+						case 4:
+							nodeDetailsContent += "\t\t\"LocationDescription\":" + value + "\",\n";
+							break;
+						case 5:
+							nodeDetailsContent += "\t\t\"Country\":" + value + "\",\n";
+							break;
+						case 6:
+							nodeDetailsContent += "\t\t\"Continent\":" + value + "\",\n";
+							break;
+						case 7:
+							if (!value.contains("\"\"") && !value.contains("NOT-SET")) {
+								String[] coord = value.split(" ");
+								nodeDetailsContent += "\t\t\"Latitude\":" + coord[0].trim() + "\",\n"
+													+ "\t\t\"Longitude\":\"" + coord[1].trim() + "\",\n";
+							} else {
+								nodeDetailsContent += "\t\t\"Latitude\":" + "\"\" " + "\",\n"
+													+ "\t\t\"Longitude\":\"" + "\"\" " + "\",\n";
+							}
+							break;
+						case 8:
+							nodeDetailsContent += "\t\t\"ProjectType\":" + value + "\",\n";
+							break;
+						case 9:
+							nodeDetailsContent += "\t\t\"PingServer\":" + value + "\",\n";
+							break;
+						case 10:
+							nodeDetailsContent += "\t\t\"TraceServer\":" + value + "\",\n";
+							break;
+						case 11:
+							nodeDetailsContent += "\t\t\"DataServer\":" + value + "\",\n";
+							break;
+						case 12:
+							nodeDetailsContent += "\t\t\"NodeURL\":" + value + "\",\n";
+							break;
+						case 13:
+							nodeDetailsContent += "\t\t\"NodeGMT\":" + value + "\",\n";
+							break;
+						case 14:
+							nodeDetailsContent += "\t\t\"Group\":" + value + "\",\n";
+							break;
+						case 15:
+							nodeDetailsContent += "\t\t\"AppUser\":" + value + "\",\n";
+							break;
+						case 16:
+							nodeDetailsContent += "\t\t\"ContactInformation\":" + value + "\",\n";
+							break;
+						case 17:
+							if (value.contains("\n")) {
+								value = value.replace("\n", " ");
+							}
+							nodeDetailsContent += "\t\t\"NodeComments\":" + value.substring(0, value.length()-1) + "\n";	//The last line of node description doesn't end with comma
+							break;
+						default:
+							break;
 					}
 				}
-				nodeDetailsContent += "\t}\n\n";
+				nodeDetailsContent += "\t},\n\n";
 
 			} catch (Exception e) {
 				Logger.error(e);
@@ -158,8 +159,8 @@ public class CreateNodeDetailsJson {
 			}
 		}	
 
-		Utils.writeIntoFile(urlContent, C.PERL_DIR+"nodes.cf");
-		Utils.writeIntoFile(nodeDetailsContent, C.NODEDETAILS_JSON_FILE);
+		Utils.writeIntoFile(urlContent, "./data/perl/nodes.cf");
+		Utils.writeIntoFile(nodeDetailsContent, "./data/json/NodeDetails.json");
 	}
 
 }
